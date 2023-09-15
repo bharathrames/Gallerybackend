a554f0c92c0fb29cd27e46fcb5260182867727cb
bharathrames <bharath91505@gmail.com>
2023-09-15 10:33:44 +0530

    route

--- models/imageModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageSchema = new mongoose.Schema({
-  filename: String,
-  path: String,
-});
-
-module.exports = mongoose.model('Image', imageSchema);
